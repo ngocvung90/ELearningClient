7710cbb407d5cd6e6b44518bcd88d8e10f736e17
Phung Ngoc Vung <ngocvung.dt3.k53@gmail.com>
2016-06-20 00:57:12 +0700

    Temporary finish master page, prepare for designing detail page

---
+++ ElearningClient/ElearningClient/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using ElearningClient.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using ImageCircle.Forms.Plugin.Abstractions;
+
+namespace ElearningClient
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new MainView();
+        }
+    }
+}
---
+++ ElearningClient/ElearningClient/View/SettingUserView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ImageCircle.Forms.Plugin.Abstractions;
+
+using Xamarin.Forms;
+
+namespace ElearningClient.View
+{
+    public partial class SettingUserView : ContentView
+    {
+        public SettingUserView()
+        {
+            InitializeComponent();
+        }
+    }
+}
